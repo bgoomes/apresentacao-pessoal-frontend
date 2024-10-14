078163797d2bed99a00abd099e1c8bb4bbd7939f
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-10-14 14:14:44 -0300

    add links

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,7 +1,16 @@
+import { ArrowRight } from "lucide-react"
+ 
 export function Links(){
     return (
-        <div className="w-full h-screen flex items-center justify-center">
-            <h1>Links</h1>
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+                <div className="flex items-center justify-center uppercase">
+                    <ul>
+                        <li className="bg-slate-500 p-4 w-96 rounded-lg mb-4 flex justify-between hover:bg-slate-400 cursor-pointer">Instagram <ArrowRight /></li>
+                        <li className="bg-slate-500 p-4 w-96 rounded-lg mb-4 flex justify-between hover:bg-slate-400 cursor-pointer">LinkedIn <ArrowRight /></li>
+                        <li className="bg-slate-500 p-4 w-96 rounded-lg mb-4 flex justify-between hover:bg-slate-400 cursor-pointer">GitHub <ArrowRight /></li>
+                        <li className="bg-slate-500 p-4 w-96 rounded-lg mb-4 flex justify-between hover:bg-slate-400 cursor-pointer">Whatsapp <ArrowRight /></li>
+                    </ul>
+            </div>
         </div>
     )
 }
